d4d6afb7e471c7f78b0013cb7c99ae74f93e3d17
Leonardo Cabral <leonardoleocabral@outlook.com>
2022-11-20 18:01:01 -0300

    feat: user data interface

---
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec..ts
@@ -0,0 +1,15 @@
+import { UserData } from "./user-data";
+
+describe("Register user on mailing list", () => {
+  test("should add user with complete data to mailing list", async () => {
+    const users: UserData[] = [];
+    console.log(users)
+    // const repo: UerRepository = new InMemoryUserRepository(users);
+    // const useCase = new RegisterUserOnMailingList(repo);
+    // const name = "Victoria";
+    // const email = "Victoria@email.com";
+    // const response = await useCase.registerUserOnMailingList({ name, email });
+    // const user = repo.findUserByEmail(email);
+    // expect((await user).name).toBe(name);
+  });
+});
